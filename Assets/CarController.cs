b97ef41e8a95787f6741a1eb2938bb0f5a04f2b1
tsuny221 <kingoftsuny@gmail.com>
2020-08-25 18:10:41 +0900

    [Finish] project

---
+++ Assets/CarController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0;
+    Vector2 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //マウスがクリックされたら
+        {
+            //マウスをクリックした座標
+            this.startPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            //マウスを離した時
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+
+            //スワイプの長さを初速度に変換する
+            this.speed = swipeLength / 500.0f;
+
+            //効果音再生
+            GetComponent<AudioSource> ().Play();
+        }
+        transform.Translate(this.speed, 0, 0); //移動
+        this.speed *= 0.98f; //減速
+    }
+}
---
+++ Assets/GameDirector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //UI部品を使うために必要！
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+
+    void Start()
+    {
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+    }
+
+    void Update()
+    {
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+        if(length >= 0)
+        {
+            this.distance.GetComponent<Text> ().text = "ゴールまで" + length.ToString("F2") + "m";
+        }
+        else
+        {
+            this.distance.GetComponent<Text> ().text = "ゲームオーバー";
+        }
+
+    }
+}
